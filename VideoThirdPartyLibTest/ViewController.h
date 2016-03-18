1cb6b3dfd28be99531945dbca24d54086b3c9964
cangjie <cangjie@hotmail.com>
2016-03-18 16:34:51 +0800

    Initial Commit

---
+++ VideoThirdPartyLibTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoThirdPartyLibTest
+//
+//  Created by 苍杰 on 3/18/16.
+//  Copyright © 2016 cangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
