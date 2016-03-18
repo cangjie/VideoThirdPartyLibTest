2ebfa75d9e0e3c2707878d56c72c688e82458bb7
cangjie <cangjie@hotmail.com>
2016-03-18 17:02:52 +0800

    project init

--- VideoThirdPartyLibTest/AppDelegate.m
+++ VideoThirdPartyLibTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "HomeController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[HomeController alloc] initWithNibName:nil bundle:nil]];
+    
     return YES;
 }
 
