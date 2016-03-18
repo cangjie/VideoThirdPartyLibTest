6a251495e521bc9494c47ce41c0e248654579418
cangjie <cangjie@hotmail.com>
2016-03-18 18:24:48 +0800

    project init

--- VideoThirdPartyLibTest/HomeController.h
+++ VideoThirdPartyLibTest/HomeController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeController.h
 //  VideoThirdPartyLibTest
 //
 //  Created by 苍杰 on 3/18/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeController : UIViewController
 
 @end
-
